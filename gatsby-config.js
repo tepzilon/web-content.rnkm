beab79ad8509bd0e0cf425c068adf892e97239be
Techid Janphaka <techid2000@gmail.com>
2019-06-06 01:05:21 +0700

    predev-phase

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,4 +6,20 @@
 
 module.exports = {
   /* Your site config here */
+  plugins: [
+    `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-transformer-json`,
+      options: {
+        path: `${__dirname}/src/data`
+      }
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `data`,
+        path: `${__dirname}/src/data`,
+      },
+    },
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,34 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+// import { Button } from '@material-ui/core'
+// import DeleteOutline from '@material-ui/icons/DeleteOutline'
+
+import indexStyles from "./index.module.scss"
+import { graphql, StaticQuery } from "gatsby"
+
+export default () => (
+  <StaticQuery
+    query={graphql`
+      query {
+        allDataJson {
+          nodes {
+            thailandPM {
+              id,
+              name
+            }
+          }
+        }
+      }
+    `}
+    render={data => (
+      <div className={indexStyles.style1}>
+        <h3>รายชื่อนายก</h3>
+        <ul>
+          {data.allDataJson.nodes[0].thailandPM.map(x => (
+            <li key={x.id}>{x.name}</li>
+          ))}
+        </ul>
+      </div>
+    )}
+  />
+)
