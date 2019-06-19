08c770cb5fb18ee1bbeb4d2256c975a028cc7d78
Techid Janphaka <techid2000@gmail.com>
2019-06-19 15:39:54 +0700

    fixed scroll

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,6 +21,15 @@ module.exports = {
         name: `images`,
         path: `${__dirname}/src/assets/images`,
       },
+    },
+    {
+      resolve: `gatsby-plugin-nprogress`,
+      options: {
+        // Setting a color is optional.
+        color: `tomato`,
+        // Disable the loading spinner.
+        showSpinner: true,
+      },
     }
   ],
 }
--- src/components/navigationBar/navigationBar.js
+++ src/components/navigationBar/navigationBar.js
@@ -2,47 +2,31 @@ import React from 'react'
 import DesktopNav from './desktopNavBar'
 import MobileNav from './mobileNavBar'
 import { Toolbar, AppBar } from '@material-ui/core'
-import { graphql, useStaticQuery } from 'gatsby'
+import { graphql, StaticQuery } from 'gatsby'
 import { Link } from 'gatsby'
 import Img from 'gatsby-image'
 import styles from './navigationBar.module.scss'
 
-export default ({ responsive, title }) => {
-  const menus = [
-    { text: 'บ้าน', link: '/gallery', isExternal: false },
-    { text: 'ลงทะเบียน', link: 'https://www.yahoo.com/', isExternal: true },
-    { text: 'เข้าสู่ระบบ', link: 'https://www.google.com/', isExternal: true },
-    { text: 'ติดต่อเรา', link: '/', isExternal: false },
-  ]
-  const navigation = responsive.tablet ? (
-    <DesktopNav menus={menus} />
-  ) : (
-    <MobileNav menus={menus} />
-  )
-
-  const data = useStaticQuery(
-    graphql`
-      query {
-        file(relativePath: { eq: "rnkm_logo_filled.png" }) {
-          childImageSharp {
-            fluid(maxWidth: 60, maxHeight: 60, quality: 100) {
-              ...GatsbyImageSharpFluid
-            }
-          }
-        }
-      }
-    `
-  )
-
-  window.onscroll = function() {
-    scrollFuntion()
+export default class NavigationBar extends React.Component {
+  componentDidMount() {
+    window.addEventListener('scroll', this.listenToScroll)
+  }
+  
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.listenToScroll)
   }
 
-  function scrollFuntion() {
-    if (
-      document.body.scrollTop > 100 ||
-      document.documentElement.scrollTop > 100
-    ) {
+  listenToScroll = () => {
+    const winScroll =
+    document.body.scrollTop || document.documentElement.scrollTop
+
+    const height =
+      document.documentElement.scrollHeight -
+      document.documentElement.clientHeight
+
+    const scrolled = winScroll / height
+
+    if (scrolled > 0.1) {
       document
         .getElementById('img-wrapper')
         .classList.remove(styles.notScrolled)
@@ -53,19 +37,49 @@ export default ({ responsive, title }) => {
     }
   }
 
-  return (
-      <AppBar position="sticky" color="default">
-        <Toolbar style={{ margin: '5px 0',minHeight:'auto' }}>
-          <Link to="/">
-          <div id="img-wrapper" className={styles.notScrolled}>
-            <Img fluid={data.file.childImageSharp.fluid} alt="rnkm logo" />
-          </div>
-          </Link>
-          <div style={{ flexGrow: 1 }}>
-            {/* <h1>| {title}</h1> */}
-            </div>
-          {navigation}
-        </Toolbar>
-      </AppBar>
-  )
+  render() {
+    const {responsive, title} = this.props
+    const menus = [
+      { text: 'บ้าน', link: '/gallery', isExternal: false },
+      { text: 'ลงทะเบียน', link: 'https://www.yahoo.com/', isExternal: true },
+      { text: 'เข้าสู่ระบบ', link: 'https://www.google.com/', isExternal: true },
+      { text: 'ติดต่อเรา', link: '/', isExternal: false },
+    ]
+    const navigation = responsive.tablet ? (
+      <DesktopNav menus={menus} />
+    ) : (
+      <MobileNav menus={menus} />
+    )
+    return (
+      <StaticQuery
+        query={graphql`
+          query {
+            file(relativePath: { eq: "rnkm_logo_filled.png" }) {
+              childImageSharp {
+                fluid(maxWidth: 60, maxHeight: 60, quality: 100) {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            } 
+          }
+        `}
+        render={data=>(
+          <AppBar position="sticky" color="default">
+            <Toolbar style={{ margin: '5px 0',minHeight:'auto' }}>
+              <Link to="/">
+              <div id="img-wrapper" className={styles.notScrolled}>
+                <Img fluid={data.file.childImageSharp.fluid} alt="rnkm logo" />
+              </div>
+              </Link>
+              <div style={{ flexGrow: 1 }}>
+                {/* <h1>| {title}</h1> */}
+                </div>
+              {navigation}
+            </Toolbar>
+          </AppBar>
+        )}
+      >
+      </StaticQuery>
+    )
+  }
 }
