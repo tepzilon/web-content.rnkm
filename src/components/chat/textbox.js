2305daabab6e2c56936dd0c100863380183d6482
Nattanon Huabnarin <nattanon.hu@gmail.com>
2019-06-30 03:53:49 +0700

    styled 404 page and added toWebp tool

--- src/components/chat/textbox.js
+++ src/components/chat/textbox.js
@@ -2,10 +2,13 @@ import React from 'react'
 import styles from './textbox.module.scss'
 
 const textbox = ({ user, text, device }) => {
+  console.log(text);
   return (
     <div className={styles.wholeWrapper} device={device}>
       <div className={styles.align} user={user}>
-        <div className={styles.textbox}>{text}</div>
+        <div className={styles.textbox}>
+          <div dangerouslySetInnerHTML={{ __html: text }} />
+        </div>
       </div>
     </div>
   )
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,15 +1,49 @@
 import React from 'react'
 import Layout from '../components/layout'
 import { getResponsive } from '../shared/js/responsive'
+import BackgroundImage from 'gatsby-background-image'
+import { useStaticQuery, graphql } from 'gatsby'
 
 export default () => {
- 
   const responsive = getResponsive()
   const device = responsive.getDevice()
-
+  const data = useStaticQuery(graphql`
+    query {
+      background: file(relativePath: { eq: "background/background.webp" }) {
+        childImageSharp {
+          fluid(maxWidth: 2000, maxHeight: 1125, quality: 100) {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
+    }
+  `)
   return (
     <Layout device={device} title="Page not found" theme="pink">
-      <div> 404 Page Not Found. </div>
+      <BackgroundImage
+        fluid={data.background.childImageSharp.fluid}
+        style={{ height: 'calc(100vh - 140px)' }}
+      >
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '100%',
+          }}
+        >
+          <div
+            style={{
+              fontFamily: "'Blanch Caps','Noto Sans',sans-serif",
+              fontSize: '15vw',
+              color: '#F58FA3',
+              textShadow: '2px 2px 0 #fff',
+            }}
+          >
+            404 Page Not Found
+          </div>
+        </div>
+      </BackgroundImage>
     </Layout>
   )
 }
