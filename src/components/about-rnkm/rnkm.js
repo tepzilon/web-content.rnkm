f7a527929ed017abc87476ad4477817ceb315389
Nattanon Huabnarin <nattanon.hu@gmail.com>
2019-06-30 17:41:08 +0700

    added timeline components

--- src/components/about-rnkm/rnkm.js
+++ src/components/about-rnkm/rnkm.js
@@ -4,18 +4,18 @@ import ScrollableAnchor from 'react-scrollable-anchor'
 
 const rnkm = ({ device }) => {
   return (
-    <ScrollableAnchor id={'about-rnkm'}>
-      <div className={styles.container} device={device}>
-        <h2 style={{ fontWeight: 'bold' }}>รับน้องก้าวใหม่คืออะไร ?</h2>
-        <p style={{ textIndent: '2rem', marginBottom: 0 }}>
-          งานรับน้องก้าวใหม่
-          ถูกจัดขึ้นเพื่อต้อนรับและเปิดโอกาสให้นิสิตได้ทำความรู้จักกับจุฬาลงกรณ์มหาวิทยาลัยในมุมมองต่าง
-          ๆ
-          รวมถึงเสริมสร้างความผูกพันธ์ระหว่างนิสิตจากทุกคณะหลากหลายชั้นปีให้ลึกซึ้งมากยิ่งขึ้น
-          ด้วยการเป็นส่วนหนึ่งในบ้านรับน้องที่มีเอกลักษณ์และทำกิจกรรมสุดพิเศษร่วมกัน
-        </p>
-      </div>
-    </ScrollableAnchor>
+    // <ScrollableAnchor id={'about-rnkm'}>
+    <div className={styles.container} device={device} id={'about-rnkm'}>
+      <h2 style={{ fontWeight: 'bold' }}>รับน้องก้าวใหม่คืออะไร ?</h2>
+      <p style={{ textIndent: '2rem', marginBottom: 0 }}>
+        งานรับน้องก้าวใหม่
+        ถูกจัดขึ้นเพื่อต้อนรับและเปิดโอกาสให้นิสิตได้ทำความรู้จักกับจุฬาลงกรณ์มหาวิทยาลัยในมุมมองต่าง
+        ๆ
+        รวมถึงเสริมสร้างความผูกพันธ์ระหว่างนิสิตจากทุกคณะหลากหลายชั้นปีให้ลึกซึ้งมากยิ่งขึ้น
+        ด้วยการเป็นส่วนหนึ่งในบ้านรับน้องที่มีเอกลักษณ์และทำกิจกรรมสุดพิเศษร่วมกัน
+      </p>
+    </div>
+    // </ScrollableAnchor>
   )
 }
 
